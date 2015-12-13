778fe15cf2faf16c95002984809f4c4cc3a56b05
bvloali <xu_wenli@outlook.com>
2015-12-13 22:01:26 +0800

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+x<-read.table("household_power_consumption.txt",header=TRUE,sep = ";")
+x$date2<-as.Date(x$Date,"%d/%m/%Y")
+x$time2<-strptime(x$Time,"%H:%M:%S")
+x$weekday<-weekdays(x$date2,abbreviate=TRUE)
+full <- paste(as.Date(x$Date,"%d/%m/%Y"), x$Time)
+x$datetime <- as.POSIXct(full)
+
+with(x, {
+  plot(Sub_metering_1~datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~datetime,col='Red')
+  lines(Sub_metering_3~datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png")
+dev.off()
